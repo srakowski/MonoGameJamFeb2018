8687334a9d79875cad0f6378f467b248b4bbfe1b
Shawn Rakowski <srakowski@live.com>
2018-02-10 21:23:41 -0600

    base solution setup

---
+++ Source/MonoGameJamFeb2018.DesktopGL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MonoGameJamFeb2018.DesktopGL
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
